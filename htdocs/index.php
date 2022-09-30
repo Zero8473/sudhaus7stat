55f495eca47b7583a8ca213133233fb352b65f36
Kevin Michl <kmichl@b-factor.de>
2022-09-30 15:09:48 +0200

    campaign section

--- htdocs/index.php
+++ htdocs/index.php
@@ -16,14 +16,11 @@
             <img src="images/S7Logo.png" alt="sudhaus7">
         </figure>
         <div id="header-nav" class="col-6">
-
                 <ul>
                     <li><a href="">Agentur</a></li>
                     <li><a href="">Portfolio</a></li>
                     <li><a href="">Blog</a></li>
                 </ul>
-
-
         </div>
         <div class="toolbar">
             <figure><img src="images/phone.png"></figure>
@@ -31,20 +28,21 @@
             <figure><img src="images/facebook.png"></figure>
             <figure><img src="images/instagram.png"></figure>
         </div>
-
     </header>
-    <div class="animated brauerei-background">
 
+    <div class="animated brauerei-background">
         <div class="container-header">
             <p class="header">Agentur für starke Kommunikation</p>
         </div>
         <div class="container-subheader">
             <p class="subheader">Stuttgart - Wien</p>
         </div>
-
-
-
     </div>
+
+    <div class="campaign-container">
+        <?php require_once 'campaign.php'; ?>
+    </div>
+
 
 </body>
 </html>
