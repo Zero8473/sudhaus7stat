8f95a0090e28e9ea65a0e4292fdc62672dc47ce0
Kevin Michl <kmichl@b-factor.de>
2022-10-10 23:32:39 +0200

    create overlap effect and center about section content

--- htdocs/content/introduction.php
+++ htdocs/content/introduction.php
@@ -1,5 +1,5 @@
 <div class="about">
-    <div class="content">
+    <div class="about-content-box">
         <div class="about-title">
             <p>Unsere Ideen-Brauerei</p>
         </div>
@@ -8,7 +8,7 @@
             <p>Wir brauen nach ihrem Geschmack</p>
         </div>
 
-        <div class="about-content">
+        <div class="about-text">
             <p>
                 Stark kommunizieren, Design kreieren und Technik schaffen ist wie das Brauen eines guten Bieres: einfach,
                 klar und ehrlich, aber gleichzeitig individuell, markant und charakterstark. Kommunikation sollte genauso sein.
@@ -21,69 +21,65 @@
     </div>
 </div>
 
-<div class="ingredients">
+<div class="ingredients-container">
 
-    <div class="ingredients-title">
-        <p>Die perfekte Braumischung</p>
+    <div class="ingredients-content-box">
+        <div class="ingredients-title">
+            <p>Die perfekte Braumischung</p>
+        </div>
+
+        <div class="ingredients-subheader">
+            <p>Unsere Zutaten</p>
+        </div>
+
+        <div class="ingredients-list">
+            <div class="strategy">
+                <div class="strategy-title">
+                    <p>Strategie</p>
+                </div>
+                <div class="strategy-description">
+                    <p>
+                        Analysen, Strategieentwicklung, Konzeption:
+                        Wir beraten und begleiten Sie umfassend und entwickeln gemeinsam mit Ihnen ein Konzept,
+                        das zu Ihren Wünschen und Bedürfnissen passt.
+                    </p>
+                </div>
+            </div>
+            <div class="design">
+                <div class="design-title">
+                    <p>Gestaltung</p>
+                </div>
+                <div class="design-description">
+                    <p>
+                        Individuelles, innovatives und kreatives Design, perfekt auf Sie zugeschnitten:
+                        Wir verleihen Ihrem Unternehmen, Produkt oder Ihrer Marke einen einzigartigen und unverwechselbaren Look!
+                    </p>
+                </div>
+            </div>
+            <div class="implementation">
+                <div class="implementation-title">
+                    <p>Umsetzung</p>
+                </div>
+                <div class="implementation-description">
+                    <p>
+                        Konzept und Design treffsicher und passgenau umgesetzt:
+                        in allen Kommunikationskanälen, die für Sie die richtigen sind – egal ob Print,
+                        Außenwerbung, Messen, Web, Social Media, u.v.m.
+                    </p>
+                </div>
+            </div>
+            <div class="technology">
+                <div class="technology-title">
+                    <p>Technik</p>
+                </div>
+                <div class="technology-description">
+                    <p>
+                        Mit Sicherheit die beste Technik: Wir setzen Ihre Projekte auf dem neuesten Stand der Technik um
+                        und tun alles für einen perfekten und sicheren Auftritt in der digitalen Welt,
+                        egal ob Website, Shop, SEO/SEM, Hosting oder Verschlüsselung von sensiblen Daten.
+                    </p>
+                </div>
+            </div>
+        </div>
     </div>
-
-    <div class="ingredients-subheader">
-        <p>Unsere Zutaten</p>
-    </div>
-
-    <div class="ingredients-list">
-        <div class="strategy">
-            <div class="strategy-title">
-                <p>Strategie</p>
-            </div>
-            <div class="strategy-description">
-                <p>
-                    Analysen, Strategieentwicklung, Konzeption:
-                    Wir beraten und begleiten Sie umfassend und entwickeln gemeinsam mit Ihnen ein Konzept,
-                    das zu Ihren Wünschen und Bedürfnissen passt.
-                </p>
-            </div>
-        </div>
-
-        <div class="design">
-            <div class="design-title">
-                <p>Gestaltung</p>
-            </div>
-            <div class="design-description">
-                <p>
-                    Individuelles, innovatives und kreatives Design, perfekt auf Sie zugeschnitten:
-                    Wir verleihen Ihrem Unternehmen, Produkt oder Ihrer Marke einen einzigartigen und unverwechselbaren Look!
-                </p>
-            </div>
-        </div>
-
-        <div class="implementation">
-            <div class="implementation-title">
-                <p>Umsetzung</p>
-            </div>
-            <div class="implementation-description">
-                <p>
-                    Konzept und Design treffsicher und passgenau umgesetzt:
-                    in allen Kommunikationskanälen, die für Sie die richtigen sind – egal ob Print,
-                    Außenwerbung, Messen, Web, Social Media, u.v.m.
-                </p>
-            </div>
-        </div>
-
-        <div class="technology">
-            <div class="technology-title">
-                <p>Technik</p>
-            </div>
-            <div class="technology-description">
-                <p>
-                    Mit Sicherheit die beste Technik: Wir setzen Ihre Projekte auf dem neuesten Stand der Technik um
-                    und tun alles für einen perfekten und sicheren Auftritt in der digitalen Welt,
-                    egal ob Website, Shop, SEO/SEM, Hosting oder Verschlüsselung von sensiblen Daten.
-                </p>
-            </div>
-        </div>
-
-    </div>
-
-
 </div>
