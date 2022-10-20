93122966fe8962fa44724bf02e21a4bb16dfba03
Kevin Michl <kmichl@b-factor.de>
2022-10-20 10:09:14 +0200

    added transition animations to elements

--- htdocs/content/variety.php
+++ htdocs/content/variety.php
@@ -1,17 +1,17 @@
 <div class="vielfalt-container">
-    <div class="vielfalt-header">
+    <div class="vielfalt-header fade-in-right anim-speed">
         <h5>Für jeden das passende</h5>
     </div>
-    <div class="vielfalt-subheader">
+    <div class="vielfalt-subheader fade-in-right anim-speed">
         <h2>Unsere Sorten-<br>Vielfalt</h2>
     </div>
-    <div class="divider-1 fade-in-left anim-slow">
+    <div class="divider-1 fade-in-right anim-slow">
         <span id="empty"></span>
         <span class="divider-element"></span>
     </div>
 </div>
 
-<div class="individual-container">
+<div class="individual-container fade-in-right anim-speed">
     <div class="individual-concept">
         <p>
             Wie beim Bier der Konsument, so hat auch jeder Kunde eigene Wünsche und Bedürfnisse
