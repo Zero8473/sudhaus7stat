fa8b33cf985e9ee6d4191f83baa4b906077fcace
Kevin Michl <kmichl@b-factor.de>
2022-11-24 12:01:02 +0100

    added CSS for contact block

--- htdocs/content/contact.php
+++ htdocs/content/contact.php
@@ -1,10 +1,10 @@
 <div class="contact-content">
     <div class="contact-info">
-        <div class="contact-title">
+        <div class="contact-title fade-in-right anim-speed">
             <p>Kommen Sie mit uns in</p>
         </div>
 
-        <div class="contact-subtitle">
+        <div class="contact-subtitle fade-in-right anim-speed">
             <h4>Kontakt</h4>
         </div>
 
@@ -13,30 +13,38 @@
             <span class="divider-element"></span>
         </div>
 
-        <div>
-            <p style="font-weight: 700">Sudhaus7</p>
+        <div class="location-container">
+            <div class="location">
+                <div>
+                    <p style="font-weight: 700">Sudhaus7</p>
+                </div>
+                <div>
+                    <p style="font-weight: 700">Agentur für starke Kommunikation</p>
+                </div>
+                <div class="stuttgart">
+                    <div class="title">
+                        <p style="font-weight: 700">Stuttgart</p>
+                    </div>
+                    <div>
+                        <span>Tel <a href="tel:+49 711.1842 02-80">+49 711.1842 02-80</a></span>
+                    </div>
+                    <p>Fax +49 711.1842 02-99</p>
+                    <a style="background-color: white; letter-spacing: 0;" href="mailto:hallo@sudhaus7.de">hallo@sudhaus7.de</a>
+                    <p>Sigmaringer Straße 98</p>
+                    <p>70567 Stuttgart</p>
+                </div>
+
+            </div>
+            <div class="location">
+                <p style="font-weight:700 ">Wien</p>
+                <p>Tel +43 720 882013</p>
+                <p>Schenkenstraße 2</p>
+                <p>1010 Wien</p>
+            </div>
         </div>
 
-        <div>
-            <p style="font-weight: 700">Agentur für starke Kommunikation</p>
-        </div>
-        <div class="location">
-            <div class="title">
-                <p style="font-weight: 700">Stuttgart</p>
-            </div>
-            <div>
-                <span>Tel <a href="tel:+49 711.1842 02-80">+49 711.1842 02-80</a></span>
-            </div>
-            <a style="background-color: white; letter-spacing: 0;" href="mailto:hallo@sudhaus7.de">hallo@sudhaus7.de</a>
-            <p>Sigmaringer Straße 98</p>
-            <p>70567 Stuttgart</p>
-        </div>
-        <div class="location">
-            <p style="font-weight:700 ">Wien</p>
-            <p>Tel +43 720 882013</p>
-            <p>Schenkenstraße 2</p>
-            <p>1010 Wien</p>
-        </div>
+
+
     </div>
 
     <div class="contact-form">
