f928ad33453951651177dcb2358648dd9c9e2959
Kevin Michl <kmichl@b-factor.de>
2022-11-29 10:37:21 +0100

    changed toggle.js - clicking on an active element minimizes it, toggle also toggles text now

--- htdocs/JavaScript/toggle.js
+++ htdocs/JavaScript/toggle.js
@@ -7,27 +7,44 @@ window.onload = function (){
 };
 
 const accordion = function (){
-    const accordionHeads = document.getElementsByClassName('toggle-title');
-    const accordionText = document.getElementsByClassName('show-description');
+    const accordionHeads = document.getElementsByClassName('toggle-title'); //array of elements that contain the class toggle-title
+    const accordionText = document.getElementsByClassName('show-description'); //array of elements that contain the class show-description
+    //adds eventlistener to all the titles
     for (let i = 0; i < accordionHeads.length; i++){
+        //EventListener will be triggered when user clicks on the respective elements
         accordionHeads[i].addEventListener('click', function(){
             console.log(accordionHeads)
-            //remove the active symbol from all elements and add the standard symbol
+            //remove the active symbol from all elements and add the standard symbol unless user clicks on active element
             for (let j = 0; j < accordionHeads.length; j++){
-              accordionHeads[j].classList.remove('symbolMinus');
-              accordionHeads[j].classList.add('symbolPlus');
+                //only executes if user clicks on an inactive element
+                if(this.classList.contains('symbolPlus')){
+                    accordionHeads[j].classList.remove('symbolMinus');
+                    accordionHeads[j].classList.add('symbolPlus');
+                }
             }
-            //add active symbol to the element the user clicked on and remove inactive symbol
-            this.classList.add('symbolMinus');
-            this.classList.remove('symbolPlus');
-
             //make the text of all inactive elements invisible
             for(let k = 0; k < accordionText.length; k++){
                 accordionText[k].classList.remove('accordion-active');
             }
+            /*console.log(this.classList);*/
+            //console.log(this.classList[1]); returns symbolPlus or SymbolMinus
 
-            //adds accordion class to sibling of active element to display text
-            this.nextElementSibling.classList.add('accordion-active');
+
+            //minimizes the active element if the user clicks on the active element or expands an inactive element if the user clicks on minimized elements
+            if(this.classList[1]==='symbolMinus'){
+                this.classList.remove('symbolMinus');
+                this.classList.add('symbolPlus');
+                this.nextElementSibling.classList.remove('accordion-active');
+            }else{
+                //add active symbol to the element the user clicked on and remove inactive symbol
+                this.classList.add('symbolMinus');
+                this.classList.remove('symbolPlus');
+
+                //adds accordion class to sibling of active element to display text
+                this.nextElementSibling.classList.add('accordion-active');
+            }
+           /* console.log(this.classList[1]);*/
+
         });
     }
 };
