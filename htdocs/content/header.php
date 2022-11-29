768822a2035a741a97a4272d6f2a1e174907bfe1
Kevin Michl <kmichl@b-factor.de>
2022-11-29 12:34:39 +0100

    replaced toolbar images with fontello icons

--- htdocs/content/header.php
+++ htdocs/content/header.php
@@ -8,9 +8,19 @@
         <li><a href="">Blog</a></li>
     </ul>
 </div>
-<div class="toolbar">
+<!--<div class="toolbar">
     <figure><img src="./Images/phone.png" alt="phone icon"></figure>
     <figure><img src="./Images/mail.png" alt="mail icon"></figure>
     <figure><img src="./Images/facebook.png" alt="facebook icon"></figure>
     <figure><img src="./Images/instagram.png" alt="instagram icon"></figure>
+</div>-->
+<div class="toolbar">
+    <div class="figure"><p class="icon-phone"></div>
+    <div class="figure"><p class="icon-mail"></div>
+    <div class="figure"><p class="icon-facebook"></div>
+    <div class="figure"><p class="icon-instagram"></div>
+
+    <!--<figure><img src="./Images/mail.png" alt="mail icon"></figure>
+    <figure><img src="./Images/facebook.png" alt="facebook icon"></figure>
+    <figure><img src="./Images/instagram.png" alt="instagram icon"></figure>-->
 </div>
