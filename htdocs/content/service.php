c74862e4c1e12b326064c4a8baecdef471ec70a4
Kevin Michl <kmichl@b-factor.de>
2022-12-01 14:15:17 +0100

    set up service footer

--- htdocs/content/service.php
+++ htdocs/content/service.php
@@ -1,23 +1,23 @@
 <div class="service-container">
     <div class="services">
         <figure class="service-elements">
-            <img src="" alt="">
+            <img src="../Images/typo3-white.svg" alt="Typo3 Logo Sudhaus7">
         </figure>
 
         <figure class="service-elements">
-            <img src="" alt="">
+            <img src="../Images/IHK_LO_AUSBILD_RZ_100K_b70_weiss-300x239.png" alt="IHK Ausbildung Sudhaus7">
         </figure>
 
         <figure class="service-elements">
-            <img src="" alt="">
+            <img src="../Images/Wordpress_weiss.svg" alt="Wordpress Logo">
         </figure>
 
         <figure class="service-elements">
-            <img src="" alt="">
+            <img src="../Images/Solr_Logo_on_black_WHITE.svg" alt="Solr Logo">
         </figure>
 
         <figure class="service-elements">
-            <img src="" alt="">
+            <img src="../Images/shopware_logo_white-RGB_VER.png" alt="shopware Logo">
         </figure>
 
     </div>
