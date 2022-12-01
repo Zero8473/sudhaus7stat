7d901deb80396221b69b67e4556cfbc34bc11109
Kevin Michl <kmichl@b-factor.de>
2022-12-01 09:14:53 +0100

    added service images to directory

--- htdocs/content/service.php
+++ htdocs/content/service.php
@@ -1,3 +1,24 @@
 <div class="service-container">
-    
+    <div class="services">
+        <figure class="service-elements">
+            <img src="" alt="">
+        </figure>
+
+        <figure class="service-elements">
+            <img src="" alt="">
+        </figure>
+
+        <figure class="service-elements">
+            <img src="" alt="">
+        </figure>
+
+        <figure class="service-elements">
+            <img src="" alt="">
+        </figure>
+
+        <figure class="service-elements">
+            <img src="" alt="">
+        </figure>
+
+    </div>
 </div>
