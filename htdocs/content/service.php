d4a2f2392bf1d054ff571ee01f6dfc604c75dba6
Kevin Michl <kmichl@b-factor.de>
2022-12-06 12:31:02 +0100

    footer bottom part updated

--- htdocs/content/service.php
+++ htdocs/content/service.php
@@ -19,6 +19,43 @@
         <figure class="service-elements">
             <img src="../Images/shopware_logo_white-RGB_VER.png" alt="shopware Logo">
         </figure>
+    </div>
+</div>
+
+<div class="rights-container">
+    <div class="rights-content">
+        <figure class="logo-mini">
+            <img src="./Images/180919_S7_Favicon.svg" alt="sudhaus7">
+        </figure>
+
+        <div class="trademark">
+            <p>© 2022 | Sudhaus7.de | All Rights Reserved</p>
+        </div>
+
+        <div class="footer-links">
+            <div class="link-1">
+                <a href="">
+                    Impressum
+                </a>
+            </div>
+
+            <div class="link-">
+                <a href="">
+                    Datenschutzerklärung
+                </a>
+            </div>
+        </div>
+
+        <div class="footer-icons-container">
+            <div class="footer-icons">
+                <p class="icon-facebook">
+            </div>
+
+            <div class="footer-icons">
+                <p class="icon-instagram">
+            </div>
+        </div>
 
     </div>
+
 </div>
