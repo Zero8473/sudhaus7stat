fcad4958a46ff6fddf8fab01f5be20919a370e01
Kevin Michl <kmichl@b-factor.de>
2023-01-16 15:25:55 +0100

    completed toolbar info textbox

--- htdocs/content/header.php
+++ htdocs/content/header.php
@@ -10,10 +10,54 @@
 </div>
 
 <div class="toolbar">
-    <div class="figure"><p class="icon-phone"></p></div>
-    <div class="figure"><p class="icon-mail"></p></div>
-    <div class="figure"><p class="icon-facebook"></p></div>
-    <div class="figure"><p class="icon-instagram"></p></div>
+    <div class="left-column to-left">
+        <div class="figure"><p class="icon-phone"></p></div>
+        <div class="figure"><p class="icon-mail"></p></div>
+        <div class="figure"><p class="icon-facebook"></p></div>
+        <div class="figure"><p class="icon-instagram"></p></div>
+    </div>
+    <div class="toolbar-info to-left2">
+        <div class="close-toolbar">
+            <div class="icon-cancel"></div>
+        </div>
+
+        <div class="toolbar-info-item">
+            <span class="icon-phone"></span>
+            <span class="toolbar-info-text">
+                Tel
+                <a href="tel:+49 711.1842 02-80" target="_blank">+49 711.1842 02-80</a>
+            </span>
+        </div>
+
+        <div class="toolbar-info-item">
+            <span class="icon-location"></span>
+            <span class="toolbar-info-text">
+                Sigmaringer Straße 98 70567 Stuttgart
+            </span>
+        </div>
+
+        <div class="toolbar-info-item">
+            <span class="icon-mail"></span>
+            <span class="toolbar-info-text">
+                <a href="mailto:nbjmup;ibmmpAtveibvt8/ef" target="_blank">E-Mail Kontakt</a>
+            </span>
+        </div>
+
+        <div class="toolbar-info-item">
+            <span class="icon-facebook"></span>
+            <span class="toolbar-info-text">
+                <a href="https://www.facebook.com/Sudhaus7.Stuttgart" target="_blank">Facebookseite</a>
+            </span>
+        </div>
+
+        <div class="toolbar-info-item">
+            <span class="icon-instagram"></span>
+            <span class="toolbar-info-text">
+                <a href="https://www.instagram.com/sudhaus7" target="_blank">Instagramprofil</a>
+            </span>
+        </div>
+    </div>
+
 </div>
 
 <div>
